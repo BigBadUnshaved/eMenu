2802d2e44669dc53926c9466fda527b7660dd73a
BigBadUnshaved <jakub.wilke@gmail.com>
2021-07-31 17:40:30 +0000

    first tests added to card app + modification of card.models

--- eMenu/card/models.py
+++ eMenu/card/models.py
@@ -1,12 +1,27 @@
 from decimal import Decimal
 
+from django.core.exceptions import ValidationError
 from django.core.validators import MaxLengthValidator, MinValueValidator
 from django.db import models
+
+def assert_text_field_length(obj, field_name='description'):
+    obj_class = obj.__class__
+    deff_attribute = getattr(obj_class, field_name)
+    max_length = deff_attribute.field.max_length
+    field_value = getattr(obj, field_name)
+    value_len = len(field_value)
+    try:
+        assert value_len <= max_length
+    except AssertionError:
+        message = 'Length of {} cannot exceed {} characters (current: {})'
+        raise ValidationError(message.format(field_name, max_length, value_len))
+
 
 class Card(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(
             blank=True,
+            max_length=1500,
             validators=[MaxLengthValidator(1500)],
             )
     creation_date = models.DateTimeField(auto_now_add=True, editable=False)
@@ -14,6 +29,10 @@ class Card(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        assert_text_field_length(self)
+        super().save(*args, **kwargs)
 
 class Dish(models.Model):
     cards = models.ManyToManyField(
@@ -26,6 +45,7 @@ class Dish(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(
             blank=True,
+            max_length=1500,
             validators=[MaxLengthValidator(1500)],
             )
     price = models.DecimalField(
@@ -35,8 +55,8 @@ class Dish(models.Model):
     )
     preparation_time = models.PositiveSmallIntegerField(help_text='in minutes')
     is_vegetarian = models.BooleanField(
+            verbose_name='vegetarian',
             default=False,
-            verbose_name='vegetarian'
     )
     creation_date = models.DateTimeField(auto_now_add=True, editable=False)
     last_change_date = models.DateTimeField(auto_now=True, editable=False)
@@ -46,3 +66,7 @@ class Dish(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        assert_text_field_length(self)
+        super().save(*args, **kwargs)
--- eMenu/card/tests.py
+++ eMenu/card/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from copy import copy
 
-# Create your tests here.
+from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
+from django.test import TestCase, SimpleTestCase
+
+from .models import Card, Dish
+
+base_card_kwargs = {
+    'name': 'menu {}',
+    'description': 'Sample description of the menu card',
+    'creation_date': None,
+    'last_change_date': None,
+}
+
+class CardTestCase(TestCase):
+    def setUp(self):
+        for i in range(10):
+            card_kwargs = copy(base_card_kwargs)
+            card_kwargs['name'] = 'menu {}'.format(i)
+            Card.objects.get_or_create(**card_kwargs)
+
+    def test_card_name_unique_validation(self):
+        menu_1 = Card.objects.get(name='menu 1')
+        card_kwargs = copy(base_card_kwargs)
+        card_kwargs['name'] = 'menu 1'
+        self.assertRaisesMessage(
+                expected_exception=IntegrityError,
+                expected_message='''duplicate key value violates unique '''
+                                 '''constraint "card_card_name_key"''',
+                callable=Card.objects.create,
+                **card_kwargs,
+        )
+
+    def test_card_name_length_validation(self):
+        card_kwargs = copy(base_card_kwargs)
+        card_kwargs['name'] = 1501 * 'a'
+        self.assertRaisesMessage(
+                expected_exception=ValidationError,
+                expected_message='''Length of {} cannot exceed {} '''
+                                 '''characters (current: {})''',
+                callable=Card.objects.create,
+                **card_kwargs,
+        )
+        pass
