41f0c2ec93a010974e275801e79e32163f649510
BigBadUnshaved <jakub.wilke@gmail.com>
2021-07-31 14:13:05 +0200

    generated initial migration for card app

--- eMenu/card/admin.py
+++ eMenu/card/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Card, Dish
+
+admin.site.register(Card)
+admin.site.register(Dish)
--- eMenu/card/models.py
+++ eMenu/card/models.py
@@ -6,20 +6,26 @@ from django.db import models
 class Card(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(
+            blank=True,
             validators=[MaxLengthValidator(1500)],
             )
     creation_date = models.DateTimeField(auto_now_add=True, editable=False)
     last_change_date = models.DateTimeField(auto_now=True, editable=False)
 
+    def __str__(self):
+        return self.name
+
 class Dish(models.Model):
     cards = models.ManyToManyField(
             Card,
+            blank=True,
             related_name='dishes',
             related_query_name='dish',
 
     )
     name = models.CharField(max_length=50)
     description = models.TextField(
+            blank=True,
             validators=[MaxLengthValidator(1500)],
             )
     price = models.DecimalField(
@@ -28,5 +34,15 @@ class Dish(models.Model):
             validators=[MinValueValidator(Decimal('0'))],
     )
     preparation_time = models.PositiveSmallIntegerField(help_text='in minutes')
+    is_vegetarian = models.BooleanField(
+            default=False,
+            verbose_name='vegetarian'
+    )
     creation_date = models.DateTimeField(auto_now_add=True, editable=False)
     last_change_date = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        verbose_name_plural = 'dishes'
+
+    def __str__(self):
+        return self.name
